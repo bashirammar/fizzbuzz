169fb3cdae3e33bf5fdace95caeacfb07c43e0a8
bashir ammar <bammarhakim92@gmail.com>
2022-07-16 13:04:55 +0700

    merubah file buzzfizz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+n = int(input("Masukkan angka yang diinginkan : "))
+
+for i in range(1, n+1):
+    if i%15==0:
+        print("fizzbuzz")
+    elif i%3==0:
+        print("fizz")
+    elif i%5==0:
+        print("buzz")
+    else:
+        print(str(i))
